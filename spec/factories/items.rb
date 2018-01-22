168ed3593934c1feb1cf4bb84547cd60c59d7a0f
ZacharyLandes <zach.g.landes@gmail.com>
2018-01-22 16:47:06 -0700

    items tests passing

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
-    name "MyString"
-    description "MyString"
+    name "candy"
+    description "food"
     unit_price "9.99"
     merchant_id 1
   end
---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+describe "Items API" do
+  it "sends a list of items" do
+    create_list(:merchant, 3)
+    create_list(:item, 3, merchant_id: Merchant.first.id)
+
+    get '/api/v1/items'
+
+    expect(response).to be_success
+  end
+
+  it "shows a single item" do
+
+    merchant = create(:merchant )
+    item = create(:item, merchant_id:merchant.id)
+
+    get "/api/v1/items/#{item.id}"
+
+    show_item = JSON.parse(response.body)
+
+
+    expect(item.name).to eq(show_item['name'])
+  end
+
+  it "can create a new item" do
+    merchant = create(:merchant )
+    item_params = { name: "Saw", description: "I want to play a game", unit_price: 99.99, merchant_id: merchant.id }
+
+    post "/api/v1/items", params: {item: item_params}
+    item = Item.last
+
+    assert_response :success
+    expect(response).to be_success
+    expect(item.name).to eq(item_params[:name])
+  end
+
+  it "can update an item" do
+    merchant = create(:merchant )
+    item = create(:item, merchant_id:merchant.id)
+    old_name = item.name
+    item_params = { name: "Saw", description: "I want to play a game", unit_price: 99.99, merchant_id: merchant.id }
+
+    put "/api/v1/items/#{item.id}", params: {item: item_params}
+    item = Item.last
+
+    assert_response :success
+    expect(response).to be_success
+    expect(item.name).to_not eq(old_name)
+    expect(item.name).to eq(item_params[:name])
+
+  end
+
+  it "can delete an item" do
+    merchant = create(:merchant )
+    item = create(:item, merchant_id:merchant.id)
+
+    delete "/api/v1/items/#{item.id}"
+
+    assert_response :success
+    expect(response).to be_success
+    expect(Item.count).to eq(0)
+  end
+end
