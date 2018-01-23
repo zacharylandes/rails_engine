933ea211a82ac06d5686cb4944bdfe12802c0b73
ZacharyLandes <zach.g.landes@gmail.com>
2018-01-22 17:23:08 -0700

    invoice items tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :items, except: [:edit, :new]
       resources :invoices, except: [:edit, :new]
-
+      resources :invoice_items, except: [:edit, :new]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
