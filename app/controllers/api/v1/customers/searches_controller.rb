341f11b9e89fb5b42489b26c2d0bad4548cc89b6
Tim Tyrrell <timomitchel23@gmail.com>
2018-01-22 18:55:16 -0700

    merchant search controller implemented

---
+++ app/controllers/api/v1/customers/searches_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Customers::SearchesController < ApplicationController 
+
+  def index
+
+  end
+
+end
---
+++ app/controllers/api/v1/merchants/searches_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::Merchants::SearchesController < ApplicationController
+
+  def index
+    #find_all
+   render json: Merchant.where(params.keys.first => params.values.first)
+  end
+
+  def show
+    #find
+    render json: Merchant.find_by(params.keys.first => params.values.first)
+  end
+
+end
