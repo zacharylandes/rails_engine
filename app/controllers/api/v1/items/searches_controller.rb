3a5c11b6559a0dbd621bd05824b6fbe63a51e34b
Tim Tyrrell <timomitchel23@gmail.com>
2018-01-23 14:05:35 -0700

    updated items strong params

--- app/controllers/api/v1/items/searches_controller.rb
+++ app/controllers/api/v1/items/searches_controller.rb
@@ -1,13 +1,18 @@
 class Api::V1::Items::SearchesController < ApplicationController
   def index
     #find_all
-   render json: Item.where(params.keys.first => params.values.first)
+   render json: Item.where(item_params)
   end
 
   def show
-
     #find
-    render json: Item.find_by(params.keys.first => params.values.first)
+    render json: Item.find_by(item_params)
   end
+
+    private
+    def item_params
+      params.permit(:id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at)
+    end
+
 
 end
