57773258fb897f6093e867192d7c4ae646b01b1b
Tim Tyrrell <timomitchel23@gmail.com>
2018-01-23 17:44:01 -0700

    routes update for merchant biz logic

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
           get '/find_all' => "searches#index"
           get '/:id/items' => "merchant_items#index"
           get '/most_revenue' => "merchants_by_revenue#index"
+          get '/most_items' => "merchants_by_items_sold#index"
         end
         namespace :customers do 
           get '/find' => "searches#show"
