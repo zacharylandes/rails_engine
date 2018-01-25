45af7ba839eb4d0194767b686a09a92abcc5623f
ZacharyLandes <zach.g.landes@gmail.com>
2018-01-24 20:16:09 -0700

    checkout

--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,22 @@ Rails.application.routes.draw do
           get '/most_revenue' => "merchants_by_revenue#index"
           get '/most_items' => "merchants_by_items_sold#index"
           get '/revenue' => "merchants_revenue_by_date#index"
+          get '/random' => "random_controller#show"
         end
         namespace :customers do
           get '/find' => "searches#show"
           get '/find_all' => "searches#index"
           get '/:id/invoices' => "customer_invoices#index"
           get '/:id/transactions' => "customer_transactions#index"
+          get '/random' => "random_controller#show"
 
         end
         namespace :transactions do
           get '/find' => "searches#show"
           get '/find_all' => "searches#index"
           get '/:id/invoice' => "transaction_invoice#show"
+          get '/random' => "random_controller#show"
+
         end
 
         resources :merchants, only: [:index, :show]
@@ -31,6 +35,7 @@ Rails.application.routes.draw do
           get '/find_all' => "searches#index"
           get '/:id/invoice_items' => "items_invoice_items#index"
           get '/:id/merchant' => "items_merchant#show"
+          get '/random' => "random_controller#show"
 
         end
         resources :items, only: [:index,:show]
@@ -39,12 +44,14 @@ Rails.application.routes.draw do
           get '/find_all' => "searches#index"
           get '/:id/transactions' => "invoice_transactions#index"
           get '/:id/items' => "invoices_items#index"
+          get '/random' => "random_controller#show"
+
         end
         resources :invoices, only: [:index,:show]
         namespace :invoice_items do
           get '/find' => "searches#show"
           get '/find_all' => "searches#index"
-          get '/random' => "searches#random"
+          get '/random' => "random_controller#show"
 
         end
         resources :invoice_items, only: [:index,:show]
