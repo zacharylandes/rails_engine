6d81b9233aae282657e0024bd70ebacee0d6a5dc
ZacharyLandes <zach.g.landes@gmail.com>
2018-01-25 12:02:31 -0700

    item order test passing

--- app/controllers/api/v1/items/searches_controller.rb
+++ app/controllers/api/v1/items/searches_controller.rb
@@ -1,16 +1,18 @@
 class Api::V1::Items::SearchesController < ApplicationController
+
   def index
    render json: Item.where(item_params)
   end
 
   def show
-    #find
-    render json: Item.find_by(item_params)
+    render json: Item.order('id ASC').find_by(item_params)
   end
 
   private
 
   def item_params
+    params[:unit_price] = (params[:unit_price].to_d*100) if params[:unit_price]
+    params[:created_at] = DateTime.parse(params[:created_at]).utc if params[:created_at]
     params.permit(:id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at)
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
+
   def self.most_revenue(quantity)
     select('items.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue').
     joins(:invoice_items).group('items.id').order('revenue DESC').limit(quantity)
